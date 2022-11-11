91c621ae8cbeb4a139b3ded92da0e2388d250922
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-11-11 09:47:58 +0600

    project static contif change

--- src/config/urls.py
+++ src/config/urls.py
@@ -28,4 +28,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, view=never_cache(serve))
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
